77b4e30b3db70293ca7a8a4ae9c777c72e6b4f69
Ben Gustafson <ben.gus.13@gmail.com>
2017-10-11 10:26:30 -0700

    Added support for selecting files via command args

--- main.c
+++ main.c
@@ -7,37 +7,33 @@
 
 int main(int argc, char* argv[])
 {
+    FILE *ptr_file1;
+    struct ReadMaze Maze1;
+	
 	/*
-	 * Possible To-Do: Use command line args to select files
+	 * Use command line args to select files
 	 * (Instead of hard-coding the names; more flexible)
 	 */
-    FILE *ptr_file1 = fopen("maze2.txt", "r");
-    FILE *ptr_file2 = fopen("maze3.txt", "r");
+	if (argc > 1)
+		ptr_file1 = fopen(argv[1], "r");
+	else
+	{
+		printf("\n** NO ARGS, Using maze2.txt **\n-> Use './program [file]'\n\n");
+		ptr_file1 = fopen("maze2.txt", "r");
+	}
 
     /* Read in Maze Data - Maze 1 is maze2.txt and Maze 2 is maze3.txt */
-    struct ReadMaze Maze1, Maze2;
     Maze1 = Copy_data(Maze1, Read_Size(ptr_file1));
     Display(Maze1.MazeData, Maze1.Columns, Maze1.rows);
 
     printf("\n\n\n");
-
-    Maze2 = Copy_data(Maze2, Read_Size(ptr_file2));
-    Display(Maze2.MazeData, Maze2.Columns, Maze2.rows);
-
-	printf("\n\n\n");
-
+	
     fclose(ptr_file1);
-    fclose(ptr_file2);
 
 	SolveMaze(Maze1);
-	printf("\n");
-    SolveMaze(Maze2);
 
     free(Maze1.start);
-    free(Maze2.start);
     free(Maze1.end);
-    free(Maze2.end);
     Destroyer(Maze1.MazeData, Maze1.Columns, Maze1.rows);
-    Destroyer(Maze2.MazeData, Maze2.Columns, Maze2.rows);
     return 0;
 }
